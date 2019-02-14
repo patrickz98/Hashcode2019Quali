331408ede06c1ea50f15e62e01c731a1a3574e6d
patrickz98 <patrickz98@users.noreply.github.com>
2019-02-14 17:27:50 +0100

    Rename.

---
+++ go/slicer/search.go
@@ -0,0 +1,27 @@
+package slicer
+
+import (
+	"../pizza"
+)
+
+func SearchSlices(pizza *pizza.Pizza) {
+
+	slicer := Slicer{Pizza: pizza}
+
+	slicer.Init()
+	// slicer.FindBiggestParts()
+	// slicer.FindSmallestParts()
+	slicer.ExpandThroughDestruction()
+
+	pizza.PrintSlices()
+	pizza.PrintScore()
+
+	// pizza.PrintSlicesToFile("xxx.txt")
+
+	// start = &pizza.PizzaPart{
+	// 	Pizza: pizz,
+	// 	VectorR: pizza.Vector{Start: 13, End: 24},
+	// 	VectorC: pizza.Vector{Start: 32, End: 47},
+	// 	Slices: []*pizza.Slice{},
+	// }
+}
