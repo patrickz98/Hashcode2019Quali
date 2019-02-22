f940e46649aceca2db1d35990ddd745ab2a5c5c4
Lukas Zierahn <lukas@kappa-mm.de>
2019-02-22 15:03:48 +0000

    merged with main and moved the conerSetter to its own package

--- go/slicer/slicer.go
+++ go/slicer/slicer.go
@@ -8,13 +8,6 @@ import (
 type Slicer struct {
 	Pizza      *pizza.Pizza
 	SliceCache map[pizza.Coordinate][]*pizza.Slice
-	Params     SlicerParams
-}
-
-type SlicerParams struct {
-	Name                 string
-	CornerDepth          int
-	CornerLastSliceDepth int
 }
 
 func (slicer *Slicer) Init() {
